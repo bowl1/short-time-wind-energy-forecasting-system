97de6cc3bd5cd0c85cf730cf6dd1e014d75d6cd4
bowl1 <bowenivy0@gmail.com>
2025-03-31 17:30:30 +0200

    update register server1

--- train-model/saved_model/register_server.py
+++ train-model/saved_model/register_server.py
@@ -5,7 +5,7 @@ import mlflow.sklearn
 mlflow.set_tracking_uri("http://20.234.40.109:5001") 
 mlflow.set_experiment("WindPower")
 
-model = joblib.load("train-model/saved_model/mlruns/925431245205846442/a7e549dcaa24498399e3acb332cc42ca/artifacts/traditional_model/model.pkl")
+model = joblib.load("/home/bowen/short-time-wind-energy-forecasting-system/train-model/saved_model/mlruns/925431245205846442/a7e549dcaa24498399e3acb332cc42ca/artifacts/traditional_model/model.pkl")
 
 with mlflow.start_run():
     mlflow.sklearn.log_model(
