efb0026118f8cc4624371145cad8e03641a79001
bowl1 <bowenivy0@gmail.com>
2025-04-01 17:20:38 +0200

    update model register in server6

--- train-model/saved_model/register_server.py
+++ train-model/saved_model/register_server.py
@@ -9,18 +9,16 @@ run_id = "63cb92ead13b427ab68bde2d5cc13899"
 model_name = "RandomForest_azure"
 model_artifact_path = "traditional_model"
 
-client = MlflowClient()
-
-# 👇 注册模型（如果没有）
+# 如果还没注册过模型名
 try:
     client.create_registered_model(model_name)
 except:
-    pass  # 已存在会报错，忽略即可
+    pass
 
-# ✅ 使用 run_id 和 artifact 路径重新注册版本
+# 重新注册为新版本
 client.create_model_version(
     name=model_name,
     source=f"mlruns/{experiment_id}/{run_id}/artifacts/{model_artifact_path}",
     run_id=run_id,
-    description="Re-registered from existing run with full metrics"
+    description="Re-registered from original run, with metrics & original artifact"
 )
