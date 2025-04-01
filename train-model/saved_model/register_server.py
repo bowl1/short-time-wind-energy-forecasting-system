89b78e22f543cb9bdc16cc5ccdc881cc5a6e97a7
bowl1 <bowenivy0@gmail.com>
2025-04-01 22:33:49 +0200

    update register server4

--- train-model/saved_model/register_server.py
+++ train-model/saved_model/register_server.py
@@ -2,21 +2,14 @@ import mlflow
 from mlflow.tracking import MlflowClient
 
 mlflow.set_tracking_uri("http://20.234.40.109:5001") 
-client = MlflowClient()
 
-experiment_id = "925431245205846442"
-run_id = "63cb92ead13b427ab68bde2d5cc13899"
 model_name = "RandomForest"
+run_id = "63cb92ead13b427ab68bde2d5cc13899"
+artifact_path = "traditional_model"
 
-# 如果模型还没注册过
-try:
-    client.create_registered_model(model_name)
-except:
-    pass
+model_uri = f"runs:/{run_id}/{artifact_path}"
 
-client.create_model_version(
-    name=model_name,
-    source=f"../../mlruns/{experiment_id}/{run_id}/artifacts/traditional_model",
-    run_id=run_id,
-    description="Re-registered from existing run with metrics"
+mlflow.register_model(
+    model_uri=model_uri,
+    name=model_name
 )
