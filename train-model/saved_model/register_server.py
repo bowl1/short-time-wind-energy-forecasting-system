4e71bcf1f6826eebb9fb8a0a1abfa5c3f39e22ad
bowl1 <bowenivy0@gmail.com>
2025-04-01 16:55:09 +0200

    update model register in server3

--- train-model/saved_model/register_server.py
+++ train-model/saved_model/register_server.py
@@ -4,20 +4,19 @@ from mlflow.tracking import MlflowClient
 mlflow.set_tracking_uri("http://20.234.40.109:5001") 
 client = MlflowClient()
 
-run_id = "63cb92ead13b427ab68bde2d5cc13899"
-model_path = "traditional_model"  # artifact 路径
 experiment_id = "925431245205846442"
+run_id = "a7e549dcaa24498399e3acb332cc42ca"
+model_path = "traditional_model"
+source_path = f"mlruns/{experiment_id}/{run_id}/artifacts/{model_path}"
 
-# 注册模型（如果没有注册过可以加上）
 try:
     client.create_registered_model("RandomForest_azure")
 except:
-    pass  # 已经存在会报错，忽略即可
+    pass  # 如果已存在，会报错，可以忽略
 
-# 创建模型版本，引用原始 run 的 artifact
 client.create_model_version(
-    name="RandomForest",
-    source=f"mlruns/925431245205846442/a7e549dcaa24498399e3acb332cc42ca/artifacts/traditional_model",
+    name="RandomForest_azure",
+    source=source_path,
     run_id=run_id,
-    description="Re-registered from original training run  with metrics"
+    description="Re-register from existing run"
 )
