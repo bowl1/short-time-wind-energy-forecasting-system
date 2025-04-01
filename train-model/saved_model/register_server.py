7eaef9f284a5ab47eadd8f244b7f7a26974a255a
bowl1 <bowenivy0@gmail.com>
2025-04-01 15:35:56 +0200

    update register server

--- train-model/saved_model/register_server.py
+++ train-model/saved_model/register_server.py
@@ -1,15 +1,23 @@
-import joblib
 import mlflow
-import mlflow.sklearn
+from mlflow.tracking import MlflowClient
 
-mlflow.set_tracking_uri("http://20.234.40.109:5001") 
-mlflow.set_experiment("WindPower")
+mlflow.set_tracking_uri("http://127.0.0.1:5001")  # or http://mlflow:5000 inside Docker
+client = MlflowClient()
 
-model = joblib.load("/home/bowen/short-time-wind-energy-forecasting-system/train-model/saved_model/mlruns/925431245205846442/a7e549dcaa24498399e3acb332cc42ca/artifacts/traditional_model/model.pkl")
+run_id = "63cb92ead13b427ab68bde2d5cc13899"
+model_path = "traditional_model"  # artifact 路径
+experiment_id = "925431245205846442"
 
-with mlflow.start_run():
-    mlflow.sklearn.log_model(
-        model,
-        artifact_path="model",
-        registered_model_name="RandomForest"
-    )
+# 注册模型（如果没有注册过可以加上）
+try:
+    client.create_registered_model("RandomForest_azure")
+except:
+    pass  # 已经存在会报错，忽略即可
+
+# 创建模型版本，引用原始 run 的 artifact
+client.create_model_version(
+    name="RandomForest",
+    source=f"/home/bowen/short-time-wind-energy-forecasting-system/train-model/saved_model/mlruns/925431245205846442/a7e549dcaa24498399e3acb332cc42ca/artifacts/traditional_model/model.pkl",
+    run_id=run_id,
+    description="Re-registered from original training run  with metrics"
+)
