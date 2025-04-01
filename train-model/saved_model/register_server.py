86c641f60dc6cfd33cc20b427b30cdfe59839d9e
bowl1 <bowenivy0@gmail.com>
2025-04-01 23:15:35 +0200

    update register server6

--- train-model/saved_model/register_server.py
+++ train-model/saved_model/register_server.py
@@ -1,27 +1,25 @@
 import mlflow
 from mlflow.tracking import MlflowClient
 
-mlflow.set_tracking_uri("http://20.234.40.109:5001") 
-
+mlflow.set_tracking_uri("http://mlflow:5000")  # ← docker 容器内必须用服务名通信
 client = MlflowClient()
 
 experiment_id = "925431245205846442"
 run_id = "63cb92ead13b427ab68bde2d5cc13899"
 model_name = "RandomForest"
+artifact_path = "traditional_model"
 
+source = f"mlruns/{experiment_id}/{run_id}/artifacts/{artifact_path}"
 
-model_artifact_path = f"/mlflow/mlruns/{experiment_id}/{run_id}/artifacts/traditional_model"
-
-# 只注册一次模型名（已存在就跳过）
+# 注册一次
 try:
     client.create_registered_model(model_name)
-except Exception:
+except:
     pass
 
-# 注册为新版本
 client.create_model_version(
     name=model_name,
-    source=model_artifact_path,
+    source=source,
     run_id=run_id,
-    description="Re-registered from host with correct artifact path"
+    description="Registering model from existing run"
 )
